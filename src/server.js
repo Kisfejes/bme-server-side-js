0b3ea7c6697e5b2c5668da17f3911b748f728477
Mark Fejes <mark.fejes@supercharge.io>
2019-10-10 19:12:17 +0200

    initial commit

---
+++ src/server.js
@@ -0,0 +1,32 @@
+// core
+const http = require('http');
+const path = require('path');
+// 3pp
+const portfinder = require('portfinder');
+const express = require('express');
+
+const PROJECT_ROOT = path.resolve(__dirname, '..');
+
+async function main() {
+  try {
+    const port = await portfinder.getPortPromise({ port: 8080 });
+
+    const app = express();
+    // Serve bootstrap files
+    const bootstrapPath = path.join(PROJECT_ROOT, 'node_modules', 'bootstrap', 'dist');
+    app.use('/bootstrap', express.static(bootstrapPath));
+
+    // Serve static sites
+    const staticSitesPath = path.join(PROJECT_ROOT, 'static-sites');
+    app.use('/', express.static(staticSitesPath));
+
+    const server = http.createServer(app);
+    server.listen(port, () => {
+      console.log(`Server is listening on port: ${port}`);
+    });
+  } catch (error) {
+    console.log(`Server could not start because of an error: ${error.message}`);
+  }
+}
+
+main();
