c4beb85fd61cdb6c7fded01bcba6dd1f7843b45e
Mark Fejes <mark.fejes@supercharge.io>
2019-12-15 19:42:51 +0100

    add leave button in session page

--- src/middlewares/session/session-get.mw.js
+++ src/middlewares/session/session-get.mw.js
@@ -12,9 +12,15 @@ const sessionGet = async (req, res, next) => {
       !session.songs.find(sessionSong => sessionSong._id.toString() === song._id.toString()),
   );
 
+  const userInSession = !!session.participants.find(
+    // eslint-disable-next-line no-underscore-dangle
+    user => user._id.toString() === req.session.user._id.toString(),
+  );
+
   res.locals.session = session;
   res.locals.availableSongs = availableSongs;
   res.locals.user = req.session.user;
+  res.locals.userInSession = userInSession;
 
   next();
 };
